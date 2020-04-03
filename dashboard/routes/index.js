fb2e86a84851741dfc850368163bef1fc59fb7db
Baptiste Dumont <02300bd@gmail.com>
2020-04-03 16:10:22 +0200

    add twig

--- dashboard/routes/index.js
+++ dashboard/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const {TwingEnvironment, TwingLoaderFilesystem} = require('twing');
+let loader = new TwingLoaderFilesystem('./views/');
+let twing = new TwingEnvironment(loader);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  twing.render('index.html.twig', {'name': 'World'}).then((output) => {
+    res.end(output);
+  });
 });
 
 module.exports = router;
--- dashboard/routes/users.js
+++ dashboard/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const {TwingEnvironment, TwingLoaderFilesystem} = require('twing');
+let loader = new TwingLoaderFilesystem('./views/');
+let twing = new TwingEnvironment(loader);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  twing.render('users.html.twig', {'name': 'World'}).then((output) => {
+    res.end(output);
+  });
 });
 
 module.exports = router;
