39886d74985d3366d6ef195a5be5e7c798bade0c
Radatovic <radatovic.damien@gmail.com>
2020-04-09 11:14:31 +0200

    test sideBar

---
+++ dashboard/public/JS/sideBar.js
@@ -0,0 +1,4 @@
+let sideBar = document.getElementById('#icon');
+ sideBar.addEventListener('click', function () {
+     this.sideBar.style.color !== '#007cfd' ? this.sideBar.style.color = '#007cfd' : this.sideBar.style.color = '#b8b4cc';
+ });
